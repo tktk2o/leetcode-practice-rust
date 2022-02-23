047c63dc3a04006ab78577a007ac2ca286f23af8
tktk2o <tktk2o.dev@gmail.com>
2022-02-23 19:08:45 +0900

    answer binary_search

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let result = binary_search(vec![1, 2, 3, 4], 2);
+    println!("{}", result);
+}
+
+fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
+    let maybe_num = nums.iter().position(|&x| x == target);
+    match maybe_num {
+        Some(n) => n as i32,
+        None => -1,
+    }
 }
