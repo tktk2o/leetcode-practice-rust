7b60e57c694e95b44124da0b1fd0749312cf07a5
tktk2o <tktk2o.dev@gmail.com>
2022-02-23 19:17:02 +0900

    add test

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,13 @@ fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
         None => -1,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::binary_search;
+    #[test]
+    fn test_binary_search() {
+        assert_eq!(binary_search(vec![-1, 0, 3, 5, 9, 12], 9), 4);
+        assert_eq!(binary_search(vec![-1, 0, 3, 5, 9, 12], 2), -1);
+    }
+}
